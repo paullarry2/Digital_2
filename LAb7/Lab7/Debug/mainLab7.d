99bc5ec3857526b19b07030af0879ecf4667ff06
paullarry2 <78246265+paullarry2@users.noreply.github.com>
2021-05-14 01:33:24 -0600

    Creacion y Configuración reloj

---
+++ LAb7/Lab7/Debug/mainLab7.d
@@ -0,0 +1,63 @@
+# FIXED
+
+mainLab7.obj: ../mainLab7.c
+mainLab7.obj: C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h
+mainLab7.obj: C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h
+mainLab7.obj: C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h
+mainLab7.obj: C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h
+mainLab7.obj: C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h
+mainLab7.obj: C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h
+mainLab7.obj: C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h
+mainLab7.obj: C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h
+mainLab7.obj: C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h
+mainLab7.obj: C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h
+mainLab7.obj: C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdbool.h
+mainLab7.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/tm4c123gh6pm.h
+mainLab7.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h
+mainLab7.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h
+mainLab7.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h
+mainLab7.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h
+mainLab7.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h
+mainLab7.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h
+mainLab7.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/timer.h
+
+../mainLab7.c:
+
+C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h:
+
+C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h:
+
+C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h:
+
+C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h:
+
+C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h:
+
+C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h:
+
+C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h:
+
+C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdbool.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/tm4c123gh6pm.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/timer.h:
+
---
+++ LAb7/Lab7/mainLab7.c
@@ -0,0 +1,22 @@
+// Larry Paúl Fuentes
+// Carné 18117
+// Digital 2
+// Laboratorio 6
+// 14/05/2021
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/tm4c123gh6pm.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "inc/hw_ints.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/gpio.h"
+#include "driverlib/timer.h"
+
+int main(void)
+{
+    SysCtlClockSet(SYSCTL_SYSDIV_5|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
+
+}
